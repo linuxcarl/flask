1a6f1846c01e240da7e0182d0ca422eaa9c24b13
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-08 01:10:12 -0500

    se integraron templates, para generar una web

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ SE CORRE EN EL ENTORNO VIRTUAL Y DEFINIMOS VARIABLES DE ENTORNO PARA QUE FUNCION
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 """
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
+
+todos  = ['TODO 1','TODO 2','TODO 3']
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
-    response =  make_response(redirect('/hello'))
+    response = make_response(redirect('/hello'))
     response.set_cookie('user_ip', user_ip)
 
     return response
 
+
 @app.route('/hello')
 def hello():
     user_ip = request.remote_addr
-    return "Tu ip local es {}".format(user_ip)
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    return render_template('hello.html', **context)
