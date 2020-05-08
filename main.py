cb315148a5125fd74748e1ed303451e0641c992b
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-07 22:36:11 -0500

    se agrego ruta para hacer redirecto y guardar cookies

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ SE CORRE EN EL ENTORNO VIRTUAL Y DEFINIMOS VARIABLES DE ENTORNO PARA QUE FUNCION
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 """
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response =  make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return "hello flask PY"
+    user_ip = request.remote_addr
+    return "Tu ip local es {}".format(user_ip)
