62c37f65cc234a02470de54b9a4c5898d748058d
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-09 18:46:31 -0500

    authenticacion de usarios: se agrego el logout

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ export FLASK_APP=main.py
 export FLASK_DEBUG=1
 export FLASK_ENV=development
 """
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
-from flask_login import login_required
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required, current_user
+
 
 from app import create_app
 from app.forms import LoginForm
-from app.firestore_service import get_users, get_users
+from app.firestore_service import get_users, get_todos
+
 app = create_app()
-
-
-todos = ['Comprar cafe', 'Enviar solicitud de compra',
-         'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -45,17 +43,15 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
-        'login_form': login_form,
         'username': username
     }
 
