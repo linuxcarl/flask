f80af01e09aadbd197c29b4ff2dbdc8d6e189d0a
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-09 02:28:40 -0500

    se agrego pruebas, y formularios con sessiones

--- main.py
+++ main.py
@@ -3,16 +3,39 @@ activar nuestro entorno con: source venv/bin/activate
 SE CORRE EN EL ENTORNO VIRTUAL Y DEFINIMOS VARIABLES DE ENTORNO PARA QUE FUNCIONE 
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
+export FLASK_ENV=development
 """
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra',
+         'Entregar video a productor ']
+
+@app.cli.command()
+def test():
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner().run(test)
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
+
 
 @app.errorhandler(500)
 def server_error(error):
@@ -24,16 +47,30 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.remote_addr
+
+    user_ip = session.get('user_ip')
+    username = session.get('username')
+    login_form = LoginForm()
+
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con exito')
+
+        return redirect(url_for('index'))
     return render_template('hello.html', **context)
