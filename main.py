d450abcd339f4f875e928c3ec102d0545375d0bb
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-09 18:19:15 -0500

    se agrego flask login

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ export FLASK_ENV=development
 """
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask_login import login_required
 
 from app import create_app
 from app.forms import LoginForm
-from app.firestore_service import get_users
+from app.firestore_service import get_users, get_users
 app = create_app()
 
 
@@ -45,6 +46,7 @@ def index():
 
 
 @app.route('/hello', methods=['GET', 'POST'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
@@ -52,23 +54,9 @@ def hello():
 
     context = {
         'user_ip': user_ip,
-        'todos': todos,
+        'todos': get_todos(user_id=username),
         'login_form': login_form,
         'username': username
     }
-    """
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre de usario registrado con éxito!')
-
-        return redirect(url_for('index'))
-    """
-    users = get_users()
-
-    for user in users:
-        print(user.id)
-        print(user.to_dict()['password'])
 
     return render_template('hello.html', **context)
