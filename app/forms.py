ca6c62eaf35ffd1e62441de42a9561207995a433
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-10 00:58:59 -0500

    se agrego la funcionalidad para modificar tareas en el campo done, como true o false

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
 
-#agregamos otra clase para agregar las tareas
+# agregamos otra clase para agregar las tareas
 class TodoForm(FlaskForm):
     description = StringField('Descripción', validators=[DataRequired()])
     submit = SubmitField('Crear tarea')
+
+#Clase de formulario para eliminar tareas
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
+
+#Clase de formulario para poner como terminadas las tareas tareas
+class UpdateTodoForm(FlaskForm):
+    submit = SubmitField('Actualizar')
