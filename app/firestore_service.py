a57b96317688ab15aedd5a388d1c22c95aa82b8e
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-09 22:26:47 -0500

    se agrego el formulario y funcionalidad para signup o registro de usuarios

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -11,10 +11,23 @@ db = firestore.client()
 def get_users():
     return db.collection('users').get()
 
+
 def get_user(user_id):
     return db.collection('users').document(user_id).get()
+
+
+def user_put(user_data):
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
+
 
 def get_todos(user_id):
     return db.collection('users')\
         .document(user_id)\
         .collection('todos').get()
+
+
+def put_todo(user_id, description):
+    todos_collection_ref = db.collection(
+        'users').document(user_id).collection('todos')
+    todos_collection_ref.add({'description': description})
